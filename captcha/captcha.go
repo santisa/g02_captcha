56bb6310d2b0d5ca605129264c5c3ba93d39e0ea
Santi Sangarkad <santi.sangarkad@ktb.co.th>
2019-10-31 11:27:53 +0700

    Refactor to use interface

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -4,31 +4,45 @@ import "fmt"
 
 var (
 	mOperand = map[int]string{
-		0:  "zero",
-		1:  "one",
-		2:  "two",
-		3:  "three",
-		4:  "four",
-		5:  "five",
-		6:  "six",
-		7:  "seven",
-		8:  "eigth",
-		9:  "nine",
-		10: "ten",
+		0: "zero",
+		1: "one",
+		2: "two",
+		3: "three",
+		4: "four",
+		5: "five",
+		6: "six",
+		7: "seven",
+		8: "eigth",
+		9: "nine",
 	}
 	mOperator = map[int]string{
 		0: "+",
 		1: "-",
 		2: "*",
 	}
+	gender = []Gender{
+		format0{},
+		format1{},
+	}
 )
 
+type Gender interface {
+	make(format, left, operator, right int) string
+}
+
+type format0 struct {
+}
+
+type format1 struct {
+}
+
+func (f format0) make(format, left, operator, right int) string {
+	return fmt.Sprintf("%d %s %s", left, mOperator[operator], mOperand[right])
+}
+func (f format1) make(format, left, operator, right int) string {
+	return fmt.Sprintf("%s %s %d", mOperand[left], mOperator[operator], right)
+}
+
 func Gen(format, left, operator, right int) string {
-	if format == 0 {
-		return fmt.Sprintf("%d %s %s", left, mOperator[operator], mOperand[right])
-	} else if format == 1 {
-		return fmt.Sprintf("%s %s %d", mOperand[left], mOperator[operator], right)
-	} else {
-		return "Invalid 'format' (should be 0,1)"
-	}
+	return gender[format].make(format, left, operator, right)
 }
--- main.go
+++ main.go
@@ -8,17 +8,14 @@ import (
 func main() {
 	fmt.Println("Input1: 0, 1, 0, 1")
 	fmt.Println("Captcha1:", captcha.Gen(0, 1, 0, 1))
+
 	fmt.Println("Input2: 1, 1, 0, 1")
 	fmt.Println("Captcha2:", captcha.Gen(1, 1, 0, 1))
 
-	fmt.Println("Input3: 0, 1, 0, 1")
-	fmt.Println("Captcha3:", captcha.Gen(0, 1, 1, 1))
-	fmt.Println("Input4: 1, 1, 1, 1")
-	fmt.Println("Captcha4:", captcha.Gen(1, 1, 1, 1))
+	fmt.Println("Input3: 0, 1, 2, 3")
+	fmt.Println("Captcha3:", captcha.Gen(0, 1, 2, 3))
 
-	fmt.Println("Input5: 0, 1, 2, 3")
-	fmt.Println("Captcha5:", captcha.Gen(0, 1, 2, 3))
-	fmt.Println("Input6: 1, 3, 2, 1")
-	fmt.Println("Captcha6:", captcha.Gen(1, 3, 2, 1))
+	fmt.Println("Input4: 1, 3, 2, 1")
+	fmt.Println("Captcha4:", captcha.Gen(1, 3, 2, 1))
 
 }
